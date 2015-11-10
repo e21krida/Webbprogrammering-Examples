6c750c80df8c2a56dc85691c2c86118eafc13055
HGustavs <henrik.gustavsson@his.se>
2015-11-11 00:39:12 +0100

    Make Resource API Functionality

--- booking/makeresource_XML.php
+++ booking/makeresource_XML.php
@@ -1,6 +1,4 @@
 <?PHP
-			header ("Content-Type:text/xml; charset=utf-8");  
-
 			include 'dbconnect.php';
 			
 			$ID=getpostAJAX("ID");
@@ -14,13 +12,30 @@
       
 			if (empty($ID) || empty($name) || empty($type) || empty($company) || empty($location) || empty($category) || empty($size) || empty($cost)) err("Missing Form Data");
 
-			$querystring="INSERT INTO resource(ID,name, type,company,location,category,size,cost) values ('".$ID."','".$name."','".$type."','".$company."','".$location."','".$category."','".$size."','".$cost."');";
-			$innerresult=mysql_query($querystring);								
+				try{
+					$querystring="INSERT INTO resource(ID,name, type,company,location,category,size,cost) values (:ID,:NAME,:TYPE,:COMPANY,:LOCATION,:CATEGORY,:SIZE,:COST);";
+					$stmt = $pdo->prepare($querystring);
+					$stmt->bindParam(':ID',$ID );
+					$stmt->bindParam(':NAME',$name );
+					$stmt->bindParam(':TYPE',$type );
+					$stmt->bindParam(':COMPANY',$address );
+					$stmt->bindParam(':LOCATION',$email );
+					$stmt->bindParam(':CATEGORY',$auxdata );
+					$stmt->bindParam(':SIZE',$auxdata );
+					$stmt->bindParam(':COST',$auxdata );
+					$stmt->execute();
+
+					header ("Content-Type:text/xml; charset=utf-8");  
+					echo '<created status="OK"/>';
+	
+			} catch (PDOException $e) {
+					err("Error!: ".$e->getMessage()."<br/>");
+					die();
+			}
 
 			if (!$innerresult){
 				err("Insert of Resource Error");
 			}else{
-					echo '<created status="OK"/>';
 			}
 
 ?>
