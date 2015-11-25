9cc15a84140b25b4e6e2b1dd2bcdc16260bdb6e8
HGustavs <henrik.gustavsson@his.se>
2015-11-25 08:51:10 +0100

    Removed unused variables.

--- booking/makebooking_XML.php
+++ booking/makebooking_XML.php
@@ -65,7 +65,7 @@
 		
 					// Successfull booking
 					header ("Content-Type:text/xml; charset=utf-8");  
-					echo "<result category='".$category."' size='".$size."' bookingcost='".$bookingcost."' bokingclass='".$bookingclass."' remaining='".$remaining."'   />";		
+					echo "<result size='".$size."' bookingcost='".$bookingcost."' remaining='".$remaining."'   />";		
 
 		} catch (PDOException $e) {
 				err("Error!: ".$e->getMessage()."<br/>");
