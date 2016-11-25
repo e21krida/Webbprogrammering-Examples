a076e0fb7dada1b9d0b214dda4df04237b8321e4
HGustavs <henrik.gustavsson@his.se>
2016-11-25 09:25:24 +0100

    Auxdata fix in makeresource

--- booking/makeresource_XML.php
+++ booking/makeresource_XML.php
@@ -9,11 +9,12 @@
 			$category=getpostAJAX("category");
 			$size=getpostAJAX("size");
 			$cost=getpostAJAX("cost");
+			$auxdata=getpostAJAX("auxdata");
       
 			if (empty($ID) || empty($name) || empty($type) || empty($company) || empty($location) || empty($category) || empty($size) || empty($cost)) err("Missing Form Data (ID/Name/Type/Company/Location/Category/Size/Cost");
 
 				try{
-					$querystring="INSERT INTO resource(ID,name, type,company,location,category,size,cost) values (:ID,:NAME,:TYPE,:COMPANY,:LOCATION,:CATEGORY,:SIZE,:COST);";
+					$querystring="INSERT INTO resource(ID,name, type,company,location,category,size,cost,auxdata) values (:ID,:NAME,:TYPE,:COMPANY,:LOCATION,:CATEGORY,:SIZE,:COST,:AUXDATA);";
 					$stmt = $pdo->prepare($querystring);
 					$stmt->bindParam(':ID',$ID );
 					$stmt->bindParam(':NAME',$name );
@@ -23,6 +24,7 @@
 					$stmt->bindParam(':CATEGORY',$category );
 					$stmt->bindParam(':SIZE',$size );
 					$stmt->bindParam(':COST',$cost );
+					$stmt->bindParam(':AUXDATA',$auxdata );
 					$stmt->execute();
 
 					header ("Content-Type:text/xml; charset=utf-8");  
@@ -31,11 +33,6 @@
 			} catch (PDOException $e) {
 					err("Error!: ".$e->getMessage()."<br/>");
 					die();
-			}
-
-			if (!$innerresult){
-				err("Insert of Resource Error");
-			}else{
 			}
 
 ?>
