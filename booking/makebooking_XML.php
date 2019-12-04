7b63d8be8147f2164f010b5b4ce03f46de4b20b8
Henrik Gustavsson <HGustavs@users.noreply.github.com>
2019-12-04 13:46:39 +0100

    Fix for orphaned bookings
    Bookings can now only be made if resource exists in app, otherwise we get informative error message

--- booking/makebooking_XML.php
+++ booking/makebooking_XML.php
@@ -10,66 +10,80 @@
 			$status=getpostAJAX("status");
 			$position=getpostAJAX("position");
 			$auxdata=getpostAJAX("auxdata");
+			$type=getpostAJAX("type");
 
 			if($user=="UNK"||$resource=="UNK"||$date=="UNK"){
 					err("Missing Form Data: (user/resource/date)");					
 			}
 
-			try{
-										
-					// Delete temp bookings for this user
-					$querystring="DELETE FROM booking WHERE status=1 and customerID=:CUSTID;";
-					$stmt = $pdo->prepare($querystring);
-					$stmt->bindParam(':CUSTID',$user);
-					$stmt->execute();
-				
-					
-					// Retrieve size and cost from resource
-					$size=0;
-					$cost=0;
-					$querystring="SELECT * FROM resource WHERE ID=:RESID";
-					$stmts = $pdo->prepare($querystring);
-					$stmts->bindParam(':RESID',$resource);
-					$stmts->execute();
-					foreach($stmts as $key => $row){
-							$size=$row['size'];
-							$cost=$row['cost'];
-					}				
-					
-					// Count number of booked resources
-					$querystring="SELECT count(*) as counted FROM booking where resourceid=:RESID and date=:DATE";
-					$stmts = $pdo->prepare($querystring);
-					$stmts->bindParam(':RESID',$row['ID']);
-					$stmts->bindParam(':DATE',$row['Date']);
-					$stmts->execute();
-		
-					// Compute Remaining Resources for Date (equals)
-					foreach($stmts as $kkey => $row){
-							$counted=$row['counted'];
-					}	
-					$remaining=$size-$counted;
-		
-					// Save booking.
-					$querystring="INSERT INTO booking(customerID,resourceID,position,date,dateto,cost,rebate,status,auxdata) values (:USER,:RESID,:POSITION,DATE_FORMAT(:DATE,'%Y-%m-%d %H:%i'),DATE_FORMAT(:DATETO,'%Y-%m-%d %H:%i'),:COST,:REBATE,:STATUS,:AUXDATA);";
-					$stmts = $pdo->prepare($querystring);
-					$stmts->bindParam(':USER',$user);
-					$stmts->bindParam(':RESID',$resource);
-					$stmts->bindParam(':POSITION',$position);
-					$stmts->bindParam(':DATE',$date);
-					$stmts->bindParam(':DATETO',$dateto);
-					$stmts->bindParam(':COST',$cost);
-					$stmts->bindParam(':REBATE',$rebate);
-					$stmts->bindParam(':STATUS',$status);
-					$stmts->bindParam(':AUXDATA',$auxdata);
-					$stmts->execute();
-		
-					// Successfull booking
-					header ("Content-Type:text/xml; charset=utf-8");  
-					echo "<result size='".$size."' bookingcost='".$cost."' remaining='".$remaining."'   />";		
+			$querystring="SELECT count(*) AS cnt FROM resource WHERE ID=:RESID and type=:TYPE;";
+			$stmts = $pdo->prepare($querystring);
+			$stmts->bindParam(':RESID',$resource);
+			$stmts->bindParam(':TYPE',$type);
+			$stmts->execute();
+			$row = $stmts->fetch();
 
-		} catch (PDOException $e) {
-				err("Error!: ".$e->getMessage()."<br/>");
-				die();
-		}
+			if($row['cnt']==0){
+						err("Error!: Resource ".$resource." does not exist for type ".$type);
+			}else{
+					try{
+
+							// Delete temp bookings for this user
+							$querystring="DELETE FROM booking WHERE status=1 and customerID=:CUSTID;";
+							$stmt = $pdo->prepare($querystring);
+							$stmt->bindParam(':CUSTID',$user);
+							$stmt->execute();
+
+
+							// Retrieve size and cost from resource
+							$size=0;
+							$cost=0;
+							$querystring="SELECT * FROM resource WHERE ID=:RESID";
+							$stmts = $pdo->prepare($querystring);
+							$stmts->bindParam(':RESID',$resource);
+							$stmts->execute();
+							foreach($stmts as $key => $row){
+									$size=$row['size'];
+									$cost=$row['cost'];
+							}				
+
+							// Count number of booked resources
+							$querystring="SELECT count(*) as counted FROM booking where resourceid=:RESID and date=:DATE";
+							$stmts = $pdo->prepare($querystring);
+							$stmts->bindParam(':RESID',$row['ID']);
+							$stmts->bindParam(':DATE',$row['Date']);
+							$stmts->execute();
+
+							// Compute Remaining Resources for Date (equals)
+							foreach($stmts as $kkey => $row){
+									$counted=$row['counted'];
+							}	
+							$remaining=$size-$counted;
+
+							// Save booking.
+							$querystring="INSERT INTO booking(customerID,resourceID,position,date,dateto,cost,rebate,status,auxdata) values (:USER,:RESID,:POSITION,DATE_FORMAT(:DATE,'%Y-%m-%d %H:%i'),DATE_FORMAT(:DATETO,'%Y-%m-%d %H:%i'),:COST,:REBATE,:STATUS,:AUXDATA);";
+							$stmts = $pdo->prepare($querystring);
+							$stmts->bindParam(':USER',$user);
+							$stmts->bindParam(':RESID',$resource);
+							$stmts->bindParam(':POSITION',$position);
+							$stmts->bindParam(':DATE',$date);
+							$stmts->bindParam(':DATETO',$dateto);
+							$stmts->bindParam(':COST',$cost);
+							$stmts->bindParam(':REBATE',$rebate);
+							$stmts->bindParam(':STATUS',$status);
+							$stmts->bindParam(':AUXDATA',$auxdata);
+							$stmts->execute();
+
+							// Successfull booking
+							header ("Content-Type:text/xml; charset=utf-8");  
+							echo "<result size='".$size."' bookingcost='".$cost."' remaining='".$remaining."'   />";		
+
+				} catch (PDOException $e) {
+						err("Error!: ".$e->getMessage()."<br/>");
+						die();
+				}
+			
+			}
+
 
 ?>
