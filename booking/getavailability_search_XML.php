8cf9659b157f857ea33b80b2ef4e8765d4927255
Henrik Gustavsson <HGustavs@users.noreply.github.com>
2021-11-17 16:14:42 +0100

    Equals

--- booking/getavailability_search_XML.php
+++ booking/getavailability_search_XML.php
@@ -38,7 +38,7 @@
 					$stmt->bindParam(':TYPE',$type);
 					$stmt->bindParam(':RESID',$resid);
 					$stmt->execute();
-				}else if(($resid=="UNK")&&($name=="UNK")&&($location=="UNK")&&($company=="UNK")&&($fulltext!="UNK")){
+				}else if(($resid=="UNK")&&($name=="UNK")&&($location=="UNK")&&($company=="UNK")&&($fulltext=="UNK")){
 					$querystring="SELECT DATE_FORMAT(date,'%Y-%m-%d %H:%i') as date,DATE_FORMAT(dateto,'%Y-%m-%d %H:%i') as dateto,resourceID,name,location,company,size,cost,category,auxdata FROM resource,resourceavailability where resourceavailability.resourceID=resource.ID and resource.type=:TYPE order by resourceID,date";
 					$stmt = $pdo->prepare($querystring);
 					$stmt->bindParam(':TYPE',$type);
